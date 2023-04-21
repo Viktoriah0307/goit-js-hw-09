83f8c0f1fa0a8916d18d10cb9e68e1921cf61f50
Viktoriah0307 <viktoriah0307@gmail.com>
2023-04-21 19:03:14 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
-import flatpickr from 'flatpickr./scr/js/02-timer.js';
+import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
 
 document.body.style.backgroundColor = '#ece5da';
 const TIMER_DELAY = 1000;
